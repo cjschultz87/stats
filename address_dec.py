2d0c366e3966f8f484db6c67599d7838a4ef0d54
CJ <131416165+cjschultz87@users.noreply.github.com>
2025-12-16 06:26:27 -0500

    Add files via upload
    converts hexadecimal address output from windbg logfiles to a list of decimal values.

---
+++ address_dec.py
@@ -0,0 +1,67 @@
+import sys
+
+digits = "0123456789abcdef"
+
+inFile = sys.argv[1]
+
+outFile = sys.argv[3]
+
+try:
+    foxtrot = open(inFile,"r")
+    foxtrotOut = open(outFile,"w")
+except:
+    print("invalid file")
+    quit()
+
+line_start = sys.argv[2]
+
+try:
+    line_start = int(line_start)
+except:
+    print("line start must be decimal integer")
+    quit()
+    
+i = 0
+
+while i < line_start:
+    foxtrot.readline()
+    i += 1
+
+sierra = ""
+
+line = ""
+
+while True:
+    line = foxtrot.readline()
+    
+    if "0:000" in line:
+        break
+    
+    delta = 0
+    
+    i = 0
+    
+    while True:
+        if line[i] == '`':
+            i += 1
+        elif not(line[i] in digits):
+            break
+        
+        hex = 0
+        
+        i_0 = 0
+        
+        while i_0 < 2:
+            hex += digits.find(line[i]) * pow(16,1-i_0)
+            
+            i += 1
+            i_0 += 1
+        
+        delta += hex * pow(256,7-((i - 2)//2))
+    
+    sierra = str(delta)
+    
+    foxtrotOut.writelines(f"{sierra}\n")
+
+foxtrot.close()
+foxtrotOut.close()
